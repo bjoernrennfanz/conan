9828b833f21448b135035d37612396a9ecac3c80
Rennfanz, Björn <bjoern.rennfanz@baslerweb.com>
2022-11-23 10:06:57 +0100

    Bump version to 1.53.1

--- conans/__init__.py
+++ conans/__init__.py
@@ -20,6 +20,6 @@ OAUTH_TOKEN = "oauth_token"
 SERVER_CAPABILITIES = [COMPLEX_SEARCH_CAPABILITY, REVISIONS]  # Server is always with revisions
 DEFAULT_REVISION_V1 = "0"
 
-__version__ = '1.53.0'
+__version__ = '1.53.1'
 __forkversion__ = '1.53.0'
 __default_remote_type__ = 'conan'
